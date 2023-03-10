499660cb47ff59820502be93c06462caffb244b8
Ritik Rajdev <ritik_rajdev@mckinsey.com>
2023-03-10 08:54:16 +0530

    feat: redirect to login if not logged i

--- src/components/ProtectedPage/index.jsx
+++ src/components/ProtectedPage/index.jsx
@@ -1,16 +1,21 @@
 import PropTypes from 'prop-types';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LOGIN_ROUTE } from '../../constants/routes';
 import { getAuthToken } from '../../utils/auth';
 
 export default function ProtectedPage({ children }) {
   const navigate = useNavigate();
+  const [useEffectCalled, setUseEffectCalled] = useState(false);
   useEffect(() => {
     if (!getAuthToken()) {
       navigate(LOGIN_ROUTE);
     }
+    setUseEffectCalled(true);
   });
+  if (!useEffectCalled) {
+    return 'Initializing...';
+  }
   return children;
 }
 
